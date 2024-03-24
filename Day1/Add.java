bf6548a6a940047ad3177149cf1657daa46d9e86
nagaarjunan <arjunarjun64030@gmail.com>
2024-03-24 22:07:29 +0530

    day 1

--- Day1/Add.java
+++ Day1/Add.java
@@ -1,10 +1,30 @@
-package Day1;
+// Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
 
-public class Add {
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+// 1, if the character is an uppercase alphabet (A - Z).
+// 0, if the character is a lowercase alphabet (a - z).
+// -1, if the character is not an alphabet.
+
+import java.util.*;
+import java.lang.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char n=sc.next().charAt(0);
+        int a=n;
+        if(a>=65 && a<=90){
+            System.out.println("1");
+        }
+        else if (a>=97 && a<=122) {
+            System.out.println("0");
+            
+        } else {
+            System.out.println("-1");
+            
+        }
+       
     }
+
+
 }
