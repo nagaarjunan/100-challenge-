dd59461075abcca9d44aa4f96234cd008659843e
nagaarjunan <arjunarjun64030@gmail.com>
2024-03-26 23:17:04 +0530

    day 4 of 100days

---
+++ Day 3/Array_Problem3.java
@@ -0,0 +1,44 @@
+// Problem statement
+// You are given a sorted integer array 'arr' of size 'n'.
+
+
+
+// You need to remove the duplicates from the array such that each element appears only once.
+
+
+
+// Return the length of this new array.
+
+
+
+// Note:
+// Do not allocate extra space for another array. You need to do this by modifying the given input array in place with O(1) extra memory. 
+
+
+// For example:
+// 'n' = 5, 'arr' = [1 2 2 2 3].
+// The new array will be [1 2 3].
+// So our answer is 3.
+// Detailed explanation ( Input/output format, Notes, Images )
+// Sample input 1:
+// 10
+// 1 2 2 3 3 3 4 4 5 5 
+// Sample output 1:
+// 5
+
+// Sample input 2:          
+// 5
+// 1 1 1 1 1
+class Solution{
+    // static int removeDuplicates(int[] arr, int n){
+    //     int j=0;
+    //     for(int i=0;i<n-1;i++){
+    //         if(arr[i]!=arr[i+1]){
+    //             arr[j++]=arr[i];
+    //         }
+    //     }
+    //     arr[j++]=arr[n-1];
+    //     return j;
+m
+
+}
